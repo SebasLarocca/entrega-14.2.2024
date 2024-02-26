9ae3e48cdff668570464ada0c5e590c7d1c53624
Sebastian <sebastian_larocca@hotmail.com>
2024-02-25 23:40:37 -0300

    metodo para vaciar array del cart

--- daos/mongo.dao/cart.dao.js
+++ daos/mongo.dao/cart.dao.js
@@ -7,20 +7,16 @@ class CartsDao {
         })
     }
 
-    static async mongotutorialupdate(cartId, productId, cantidad) {
-
+    static async addProduct(cartId, productId, cantidad) {
         const cart = await cartModel.find({ _id: cartId })
         let exists = false
         cart[0].products.forEach(element => {
             element.product.toString() == productId ? exists = true : null
-
         });
         console.log(exists)
 
         if (!exists) {
-
             //Si el producto no existe en el carrito
-
             let result = await cartModel.updateOne(
                 { _id: cartId },
                 { $addToSet: { products: { product: productId, quantity: cantidad } } }
@@ -35,8 +31,10 @@ class CartsDao {
             const result1 = await cartModel.updateOne(query, updateDocument);
             console.log('Cantidad agregada')
         }
+    }
 
-
+    static async removeProducts(cartId) {
+        return cartModel.updateOne({_id: cartId }, { $set : {"products": [] }} );
     }
 
     static async getCarts() {
--- routes/cart.route.js
+++ routes/cart.route.js
@@ -8,23 +8,33 @@ router.get('/cart', (req, res) => {
     res.send('ok')
 })
 
-router.delete('/:cid/products/:pid', async (req, res) => {
-    let cartId = req.params.cid;
-    let productId = req.params.pid
-    const cart = await CartsDao.getCartById(cartId)
-    let cartProds = cart[0].products
-    console.log(cartProds.length)
-    let index;
-    cartProds.forEach(element => {
-        if (element.product.toString() == productId) {
-            index = cartProds.indexOf(element)
-        } else {
-            console.log('no es')
-        }
-    });
-    cartProds.splice(index, 1)
-    res.send(cart.products)
+
+router.delete('/:cid', async (req, res)=>{
+    const cartId = req.params.cid;
+    console.log(cartId)
+    const result = await CartsDao.removeProducts(cartId);
+    res.send('productos borrados')
 })
+
+//Borrar 1 producto
+
+// router.delete('/:cid/products/:pid', async (req, res) => {
+//     let cartId = req.params.cid;
+//     let productId = req.params.pid
+//     const cart = await CartsDao.getCartById(cartId)
+//     let cartProds = cart[0].products
+//     console.log(cartProds.length)
+//     let index;
+//     cartProds.forEach(element => {
+//         if (element.product.toString() == productId) {
+//             index = cartProds.indexOf(element)
+//         } else {
+//             console.log('no es')
+//         }
+//     });
+//     cartProds.splice(index, 1)
+//     res.send(cart.products)
+// })
 
 router.get('/carts', async (req, res) => {
     let carts = await CartsDao.getCarts()
@@ -37,7 +47,7 @@ router.put('/:cid/products/:pid', (req, res)=>{
     const cartId = req.params.cid;
     const productId = req.params.pid;
     const quantity = req.body.quantity
-    CartsDao.mongotutorialupdate(cartId, productId, quantity)
+    CartsDao.addProduct(cartId, productId, quantity)
     res.send('Prodcuto agregado')
 })
 export default router;
